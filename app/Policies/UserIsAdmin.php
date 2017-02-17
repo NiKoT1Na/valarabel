e1b5cadef2345f24fc986a07c1938718e6439621
Nicolay Amaya <nikoamayap@gmail.com>
2017-02-16 20:08:15 -0500

    añadiendo las imagenes de las estrellas y empezando con las policys

---
+++ app/Policies/UserIsAdmin.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Policies;
+
+use App\User;
+use App\Prod;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class UserIsAdmin
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view the prod.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Prod  $prod
+     * @return mixed
+     */
+    public function view(User $user, Prod $prod)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can create prods.
+     *
+     * @param  \App\User  $user
+     * @return mixed
+     */
+    public function create(User $user)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can update the prod.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Prod  $prod
+     * @return mixed
+     */
+    public function update(User $user, Prod $prod)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can delete the prod.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Prod  $prod
+     * @return mixed
+     */
+    public function delete(User $user, Prod $prod)
+    {
+        //
+    }
+}
